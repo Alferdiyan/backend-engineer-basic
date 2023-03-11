8ad93183bc096ea85c9e70ef00f20176b4b17e43
Alferdiyan <ferdi1505@gmail.com>
2023-03-11 08:19:32 +0700

    exercise

---
+++ basic-javascript/2-struktur-data/9kuis1.js
@@ -0,0 +1,16 @@
+/// kuis object
+
+const restaurant = {
+  name: 'Noodle House',
+  city: 'Jakarta',
+  favoriteDrink: 'Iced Tea',
+  favoriteFood: 'Noodle Soup',
+  isVegan: false,
+};
+console.log('hasil');
+
+const name = restaurant.name;
+console.log(name);
+
+const favoriteDrink = restaurant.favoriteDrink;
+console.log(favoriteDrink);
