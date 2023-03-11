623a3024ce438ee08449f4d537f3aefafc524fb9
Alferdiyan <ferdi1505@gmail.com>
2023-03-11 08:13:02 +0700

    finishing fundamental

---
+++ basic-javascript/1-fundamental-js/10kuis.js
@@ -0,0 +1,15 @@
+// const stock = 0;
+// const milkNeeded = 25;
+
+// if (stock > milkNeeded) {
+//   stock = stock - milkNeeded;
+//   console.log('Processing your order...');
+// } else {
+//   console.log('Out of Stock!');
+// }
+
+// console.log('Thank you');
+let x;
+x = 7;
+x = 'JS is great';
+console.log(x);
